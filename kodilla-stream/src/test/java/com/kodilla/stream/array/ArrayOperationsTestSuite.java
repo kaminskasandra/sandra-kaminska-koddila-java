5aa55921a8da6db88f9304c837ba71f817b4c95e
kaminskasandra <san.porombka@gmail.com>
2023-11-17 16:19:18 +0100

    7.5 task correction

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -6,7 +6,8 @@ public class ArrayOperationsTestSuite {
     @Test
     void testGetAverage(){
         //Given
-        int[] numbersForTest = new int[20];
+        int[] numbersForTest = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
         //When
        double result = ArrayOperations.getAverage(numbersForTest);
         //Then

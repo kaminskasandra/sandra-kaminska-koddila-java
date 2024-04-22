4a53d56836fe286b8da19796044a43dc4deacab3
kaminskasandra <san.porombka@gmail.com>
2024-04-22 18:35:58 +0200

    preparing environment

--- kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
+++ kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
@@ -13,11 +13,11 @@ public enum DbManager {
 
     DbManager() {
         Properties connectionProps = new Properties();
-        connectionProps.put("user", "kodilla_user");
-        connectionProps.put("password", "kodilla_Pass123");
+        connectionProps.put("user", "root");
+        connectionProps.put("password", "rd82M33o");
         try {
             conn = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/kodilla_course" +
+                    "jdbc:mysql://localhost:3306/kodilla_course_module_29" +
                             "?serverTimezone=Europe/Warsaw" +
                             "&useSSL=False",
                     connectionProps);

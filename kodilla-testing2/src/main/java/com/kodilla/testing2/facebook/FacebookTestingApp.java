7a2d9e815bfc7f4039b5a4d5fdd301254b2837de
kaminskasandra <san.porombka@gmail.com>
2024-04-29 19:06:44 +0200

    task Testing Facebook app - update

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -6,29 +6,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
-import java.time.Duration;
-
 public class FacebookTestingApp {
 
-    public static final String XPATH_CREATE_ACCOUNT = "//a[text()='Utwórz nowe konto']";
+    public static final String XPATH_CREATE_ACCOUNT = "/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[5]/a";
     public static final String XPATH_DAY = "//select[@id='day']";
     public static final String XPATH_MONTH = "//select[@id='month']";
     public static final String XPATH_YEAR = "//select[@id='year']";
 
-
-    public static void main(String[] args) {
-        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://www.facebook.com/");
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        Thread.sleep(3000);
 
-        WebElement acceptCookiesButton = driver.findElement(By.xpath("  //*[@id=\"u_0_k_+8\"]"));
+        WebElement acceptCookiesButton = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div/div/div[4]/button[2]"));
         if (acceptCookiesButton.isDisplayed()) {
             acceptCookiesButton.click();
         }
 
         WebElement createAccount = driver.findElement(By.xpath(XPATH_CREATE_ACCOUNT));
         createAccount.click();
+
+        Thread.sleep(3000);
 
         WebElement dayDropdown = driver.findElement(By.xpath(XPATH_DAY));
         Select daySelect = new Select(dayDropdown);
